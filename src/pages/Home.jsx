a20a6b1a517dcc0ba783618e1d381722195b365d
Preksler <preksler@gmail.com>
2022-07-24 16:06:01 +0300

    add go back

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,14 @@
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import { useState, useEffect } from 'react';
 import { getTrendingMovies } from "services/moviesApi";
 
 const Home = () => {
     const [movies, setMovies] = useState([]);
+    const location = useLocation();
+
     useEffect(() => {
         getTrendingMovies().then(movies => setMovies(movies));
-    }
-        , []);
+    }, []);
     
     return (
         <div>
@@ -15,7 +16,9 @@ const Home = () => {
             <ul>
                 {movies.map(movie => (
                     <li key={movie.id}>
-                        <Link to={`/movies/${movie.id}`}>{movie.title}</Link>
+                        <Link to={`/movies/${movie.id}`} state={{ from: location}} >
+                            {movie.title}
+                        </Link>
                     </li>
                 ))}
             </ul>
--- src/pages/MovieInfo.jsx
+++ src/pages/MovieInfo.jsx
@@ -1,12 +1,16 @@
 import { useEffect, useState } from 'react';
-import { useParams, Link, Routes, Route } from 'react-router-dom';
+import { useParams, Link, Routes, Route, useLocation } from 'react-router-dom';
 import { getMovieById } from 'services/moviesApi';
 import Cast from '../components/Cast';
 import Reviews from '../components/Reviews';
+import { BackLink } from '../components/BackLink';
 
 const MovieInfo = () => {
   const { movieId } = useParams();
   const [movie, setMovie] = useState(null);
+
+  const location = useLocation();
+  const backLinkHref = location.state?.from ?? "/";
 
   useEffect(() => {
     getMovieById(movieId).then(movie => setMovie(movie));
@@ -15,24 +19,27 @@ const MovieInfo = () => {
   return (
     <div>
       {movie && (
-        <div>
-          <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title} />
-          <h1>{movie.title} ({new Date(movie.release_date).getFullYear()})</h1>
-          <span>User Score: {Math.round((movie.vote_average) * 10)}% </span>
-          <p>Overview</p>
-          <p>{movie.overview}</p>
-          <p>Ganres</p>
-          <p>{movie.genres.map(genre => (genre.name + ' '))}</p>
-          <p>Additional information</p>
-          <ul>
-            <li>
-              <Link to="cast">Cast</Link>
-            </li>
-            <li>
-              <Link to="reviews">Reviews</Link>
-            </li>
-          </ul>
+        <>
+          <BackLink to={backLinkHref}>Go back</BackLink>
+            <div>
+            <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title} />
+            <h1>{movie.title} ({new Date(movie.release_date).getFullYear()})</h1>
+            <span>User Score: {Math.round((movie.vote_average) * 10)}% </span>
+            <p>Overview</p>
+            <p>{movie.overview}</p>
+            <p>Ganres</p>
+            <p>{movie.genres.map(genre => (genre.name + ' '))}</p>
+            <p>Additional information</p>
+            <ul>
+              <li>
+                <Link to="cast">Cast</Link>
+              </li>
+              <li>
+                <Link to="reviews">Reviews</Link>
+              </li>
+            </ul>
         </div>
+        </>
       )}
       <Routes>
         <Route path="cast" element={<Cast movieId={movieId} />} />
